7c652620be40a6b5d221dee16c01320ce4e32be6
Rami.B <ramibalaghi@hotmail.com>
2025-02-03 23:53:21 +0100

    Created layout component

--- Rami-B/src/App.jsx
+++ Rami-B/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="portfolio">
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
